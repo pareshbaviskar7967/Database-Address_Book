1240bf23449ecde79da4f47022d7c43dee80d7b0
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-02 05:58:45 +0530

    Solution for UC-2

--- addressbook.sql
+++ addressbook.sql
@@ -4,3 +4,15 @@
 CREATE DATABASE address_book_service;
 USE address_book_service;
 
+#UC-2
+CREATE TABLE address_book (
+  Id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Fname VARCHAR(50) NOT NULL,
+  Lname VARCHAR(50) NOT NULL,
+  Address VARCHAR(50) NOT NULL,
+  City VARCHAR(50) NOT NULL,
+  State VARCHAR(50) NOT NULL,
+  Pin INT NOT NULL,
+  Mobile VARCHAR(17) NOT NULL UNIQUE,
+  Email VARCHAR(50)
+);
