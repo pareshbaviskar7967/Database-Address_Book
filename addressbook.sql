66010ea8cb2c47fe282fc744e3e22bc7b90c2789
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-03 05:58:45 +0530

    Solution for UC-3

--- addressbook.sql
+++ addressbook.sql
@@ -16,3 +16,12 @@ CREATE TABLE address_book (
   Mobile VARCHAR(17) NOT NULL UNIQUE,
   Email VARCHAR(50)
 );
+
+#UC3
+INSERT INTO address_book (Id, Fname, Lname, Address, City, State, Pin, Mobile, Email)
+VALUES (101,'Paresh','22kar','Sagar park','Jalgaon','Maharashtra',425001,'8708095441','paresh@email'),
+       (102,'Sakshi','Mujumdar','Adarsh nagar','Jalgaon','Maharashtra',425001,'987654321','sakshi@email'),
+       (103,'Animesh','Vipradas','Aryan park','Jalgaon','Maharashtra',425001,'9786542315','animesh@email'),
+       (104,'Snehal','Kale','Sagar park','Jalgaon','Maharashtra',425001,'9564984532','snehal@email'),
+       (105,'Nakul','Upadhyay','Inner ring road','Indore','Madhya Pradesh',425001,'123456789','nakul@email');
+SELECT * FROM address_book;
