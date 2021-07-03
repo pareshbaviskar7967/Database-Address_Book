8276f886b96e452c2c8a2379ca27295907b47970
pareshbaviskar7967 <pareshbaviskar7967@gmail.com>
2021-07-03 05:58:45 +0530

    Solution for UC-6

--- addressbook.sql
+++ addressbook.sql
@@ -40,3 +40,6 @@ SELECT * FROM address_book;
 DELETE FROM address_book
 WHERE Fname = 'Pratham';
 
+#UC6
+SELECT * FROM address_book WHERE City = 'Ahmadnagar';
+SELECT * FROM address_book WHERE State = 'Madhya Pradesh';
